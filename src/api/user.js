dcc7cba6defa3f6fe06ce2f5ad802e7a7f6cac47
lily <mf56liulily@gmail.com>
2021-05-11 16:37:07 +0800

    [U]更新info api

--- src/api/user.js
+++ src/api/user.js
@@ -6,7 +6,7 @@ const user = {
   apiPostLogin(data){ return req('post','/users/login',data) },
   apiPostLoginSec(tgBuildingId,data){ return req('post','/users/login/'+tgBuildingId,data) },
   apiPostLogout(data){ return req('post','/vue-admin-template/user/logout',data) },
-  apiGetUserInfo(){ return req('get','/users/'+this.getUid()+'/info') }
+  apiGetUserInfo(){ return req('get','/users/'+this.getUid()) }
 }
 
 export default user
